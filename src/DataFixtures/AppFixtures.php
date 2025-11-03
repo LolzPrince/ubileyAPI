7cc52f96324efd1889497b0098d82b221dfe7e08
strom <matem9629@gmail.com>
2025-11-03 22:37:10 +0400

    refactor: transferring test database filling from fixtures to factory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,39 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\GuestList;
+use App\Entity\Tables;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create();
+
+        // Создаем столы
+        $tables = [];
+        for ($i = 1; $i <= 10; $i++) {
+            $table = new Tables();
+            $table->setNum($i);
+            $table->setDescription($faker->optional(0.7)->text(200));
+            $table->setMaxGuests($faker->numberBetween(2, 20));
+
+            $manager->persist($table);
+            $tables[] = $table;
+        }
+
+        // Создаем гостей
+        for ($i = 0; $i < 50; $i++) {
+            $guest = new GuestList();
+            $guest->setName($faker->name());
+            $guest->setIsPresent($faker->boolean());
+            $guest->setTables($faker->randomElement($tables));
+
+            $manager->persist($guest);
+        }
 
         $manager->flush();
     }
