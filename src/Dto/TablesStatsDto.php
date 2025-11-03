c81bd9211df8d6b400427037419eee4af6f51963
strom <matem9629@gmail.com>
2025-11-03 20:49:01 +0400

    feat: add tables stats DTO and Provider

---
+++ src/Dto/TablesStatsDto.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Dto;
+
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\OpenApi\Model\Operation;
+use App\State\TablesStatsProvider;
+use Symfony\Component\Serializer\Attribute\Groups;
+
+class TablesStatsDto{
+    #[Groups(['tables:read'])]
+    public int $id;
+
+    #[Groups(['tables:read'])]
+    public int $num;
+
+    #[Groups(['tables:read'])]
+    public ?int $maxGuests;
+
+    #[Groups(['tables:read'])]
+    public int $booking;
+
+    #[Groups(['tables:read'])]
+    public int $guestIsPresent;
+
+    public function __construct(
+        int $id,
+        int $num,
+        ?int $maxGuests,
+        int $booking,
+        int $guestIsPresent
+    ) {
+        $this->id = $id;
+        $this->num = $num;
+        $this->maxGuests = $maxGuests;
+        $this->booking = $booking;
+        $this->guestIsPresent = $guestIsPresent;
+    }
+}
